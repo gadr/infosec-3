d1dbbff57548425c0793a0b8848f6c5770c26a54
Guilherme Rodrigues <gadr90@gmail.com>
2013-05-07 23:36:30 -0300

    Basic api design

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,6 +5,9 @@ import play.mvc.*;
 
 import views.html.*;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Application extends Controller {
   
     public static Result index() {
@@ -25,15 +28,31 @@ public class Application extends Controller {
         return redirect("/");
     }
 
-    public static Result auth() {
+    public static Result checkLogin() {
         String username = request().body().asFormUrlEncoded().get("username")[0];
-        if ("teste".equals(username)) {
+        System.out.println(username);
+        if ("existente".equals(username)) {
             session("connected", username);
-            return redirect("/");
+            return ok("EXISTS");
+        }
+        else if ("bloqueado".equals(username)) {
+            return forbidden("BLOCKED");
         }
         else {
-            return unauthorized("ERROR");
+            return notFound("UNKNOWN");
         }
+    }
+
+    public static Result checkPassword() {
+        String[] phonemes = request().body().asFormUrlEncoded().get("phonemes");
+        System.out.println("Phonemes: " + Arrays.toString(phonemes));
+        return ok("CHECK");
+    }
+
+    public static Result checkDigitalSignature() {
+        String signature = request().body().asFormUrlEncoded().get("signature")[0];
+        System.out.println(signature);
+        return ok("CHECK");
     }
   
 }
