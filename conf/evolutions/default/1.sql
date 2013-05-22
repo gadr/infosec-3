ebeabd539dd1be24bb7e7275737821cf13e121d9
Breno Calazans <breno@skans.com.br>
2013-05-22 17:04:53 -0300

    Sql

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,29 +4,29 @@
 # --- !Ups
 
 create table Grupo (
-  gid                       bigint auto_increment not null,
+  gid                       bigint not null,
   name                      varchar(255),
   constraint pk_Grupo primary key (gid))
 ;
 
 create table InfosecLog (
-  gid                       bigint auto_increment not null,
+  gid                       bigint not null,
   username                  varchar(255),
   filename                  varchar(255),
-  date                      datetime,
+  date                      timestamp,
   message_gid               bigint,
   constraint pk_InfosecLog primary key (gid))
 ;
 
 create table Mensagem (
-  gid                       bigint auto_increment not null,
+  gid                       bigint not null,
   code                      varchar(255),
   message                   varchar(255),
   constraint pk_Mensagem primary key (gid))
 ;
 
 create table Usuario (
-  gid                       bigint auto_increment not null,
+  gid                       bigint not null,
   username                  varchar(255),
   name                      varchar(255),
   password                  varchar(255),
@@ -35,11 +35,19 @@ create table Usuario (
   access_number             integer,
   password_tries            integer,
   signature_tries           integer,
-  blocked_until             datetime,
+  blocked_until             timestamp,
   public_key_path           varchar(255),
   public_key                varbinary(255),
   constraint pk_Usuario primary key (gid))
 ;
+
+create sequence Grupo_seq;
+
+create sequence InfosecLog_seq;
+
+create sequence Mensagem_seq;
+
+create sequence Usuario_seq;
 
 alter table InfosecLog add constraint fk_InfosecLog_message_1 foreign key (message_gid) references Mensagem (gid) on delete restrict on update restrict;
 create index ix_InfosecLog_message_1 on InfosecLog (message_gid);
@@ -50,15 +58,23 @@ create index ix_Usuario_group_2 on Usuario (group_gid);
 
 # --- !Downs
 
-SET FOREIGN_KEY_CHECKS=0;
+SET REFERENTIAL_INTEGRITY FALSE;
 
-drop table Grupo;
+drop table if exists Grupo;
 
-drop table InfosecLog;
+drop table if exists InfosecLog;
 
-drop table Mensagem;
+drop table if exists Mensagem;
 
-drop table Usuario;
+drop table if exists Usuario;
 
-SET FOREIGN_KEY_CHECKS=1;
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists Grupo_seq;
+
+drop sequence if exists InfosecLog_seq;
+
+drop sequence if exists Mensagem_seq;
+
+drop sequence if exists Usuario_seq;
 
